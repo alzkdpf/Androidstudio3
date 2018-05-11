a46fb257e474a96d63f84c3a3254d2a2b7ffef30
Michael L <code21032@gmail.com>
2018-05-11 18:23:45 +0900

    update source code

--- SceneTransitions/app/src/main/java/com/ebookfrenzy/scenetransitions/SceneTransitionsActivity.java
+++ SceneTransitions/app/src/main/java/com/ebookfrenzy/scenetransitions/SceneTransitionsActivity.java
@@ -2,13 +2,21 @@ package com.ebookfrenzy.scenetransitions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Fade;
 import android.transition.Scene;
 
 import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.view.ViewGroup;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.Toast;
 
 
@@ -27,8 +35,34 @@ public class SceneTransitionsActivity extends AppCompatActivity {
         rootContainer =
                 (ViewGroup) findViewById(R.id.rootContainer);
 
-        transitionMgr = TransitionInflater.from(this)
-                .inflateTransition(R.transition.transition);
+        //transition settter
+//        transitionMgr = TransitionInflater.from(this)
+//                .inflateTransition(R.transition.transition);
+
+        //default
+//        TransitionSet myTransition = new TransitionSet();
+
+        //option 1
+//        Transition mychangeBounds = new ChangeBounds();
+
+        //option 2
+//        TransitionSet myTransition = new TransitionSet();
+//        myTransition.addTransition(new ChangeBounds());
+//        myTransition.addTransition(new Fade());
+
+        //option 3
+        TransitionSet myTransition = new TransitionSet();
+        Transition bounds = new ChangeBounds();
+        bounds.setInterpolator(new OvershootInterpolator(3.0f));
+        myTransition.addTransition(bounds);
+
+        Transition fade = new Fade();
+        fade.setDuration(3000);
+        fade.addTarget(R.id.button);
+        myTransition.addTransition(fade);
+
+
+        transitionMgr = myTransition;
 
         scene1 = Scene.getSceneForLayout(rootContainer,
                 R.layout.scene1_layout, this);
