90c5cd8bf70b56233dcddd52f8fa53ff80446ef7
Michael L <code21032@gmail.com>
2018-05-24 12:21:55 +0900

    mulitple window code add

--- MultiWindow/app/src/main/java/com/ebookfrenzy/multiwindow/FirstActivity.java
+++ MultiWindow/app/src/main/java/com/ebookfrenzy/multiwindow/FirstActivity.java
@@ -2,10 +2,13 @@ package com.ebookfrenzy.multiwindow;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -13,10 +16,34 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        if(checkFreeform() == true)
+        {
+            Toast.makeText(this,"enabled", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"disabled", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    //다중 창 통지 확인
+
+
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode, Configuration newConfig) {
+        super.onMultiWindowModeChanged(isInMultiWindowMode, newConfig);
+
+        if(isInMultiWindowMode){
+            Toast.makeText(this,"multiple window", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"single window", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void launchIntent(View view) {
         Intent i = new Intent(this, SecondActivity.class);
+
+
 
         i.addFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT|
                 Intent.FLAG_ACTIVITY_MULTIPLE_TASK|
@@ -27,5 +54,10 @@ public class FirstActivity extends AppCompatActivity {
         ActivityOptions bounds = options.setLaunchBounds(rect);
 
         startActivity(i, bounds.toBundle());
+    }
+
+    //자유형식 지원확인
+    public Boolean checkFreeform() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_FREEFORM_WINDOW_MANAGEMENT);
     }
 }
